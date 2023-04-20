455f597fb40591fb402af01a4f90c2bbd2108db7
Anonimo <131402655+Darkk816@users.noreply.github.com>
2023-04-20 14:50:55 -0500

    Add files via upload

---
+++ AutoBroly All Race 2023.lua
@@ -0,0 +1,47 @@
+-- [   $   ] AutoBroly by eurCript Mother :D
+Config = -- Recommended Have Jars + GOD, UI or MUI 
+{
+		Credits = true; --|| Promotes my dc sv
+		Android = false; --|| false for Saiyans, Humans... | Im making new transform sistem for Androids :D
+		Earth = false; --|| true earth / false queue
+		MaxTransform = true; --|| False For Androids
+		BypassExplosiveWave = false; --|| Coming Soon !
+	Invis = true; --|| Invis
+	ConfirmGrab = false; -- || No Added
+	AimbotBroly = true; -- || Xbox Aimbot for Broly
+	CameraFov = 160; -- || Camera Fov
+	LowGFX = true; -- || For Bad PCS put in true.
+	InfBroly = true; -- || Bypass All Kicks 
+	CarryMode = false; -- || Farm Alone or with friends, with friends put in true 
+		HideLvL = true; --|| Hides ur lvl
+		WaitTime = 0.75; -- || 0.85 Recommended 
+		AutoBeansJarsSpam = true; --|| You can put in False
+		AutoPunchLowKi = true; --|| AutoPunch when your ki is Low
+}
+-- [ $ ] MoveSpam 
+MoveSet = -- You can edit this MoveSet
+{
+"Neo Wolf Fang Fist";
+"Strong Kick";
+"Meteor Crash";
+"Wolf Fang Fist";
+"Mach Kick";
+"Kick Barrage";
+"Anger Rush";
+"Demon Flash";
+"Deadly Dance";
+"";
+}
+
+
+-- Any Problems at Discord Boy
+game:GetService("StarterGui"):SetCore("SendNotification", 
+{
+		Title = "AUTOBROLY  -BETA";
+		Text = "ANY  PROBLEMS  AT  DISCORD";
+}
+)
+
+---
+loadstring(game:HttpGet("https://pastebin.com/raw/TMfyXPgq"))()
+---
